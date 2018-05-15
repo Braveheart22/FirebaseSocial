d44dde642b89b6de4c7a3566b5bf0b0f644981cd
John Strack <Braveheart22@GMail.com>
2018-05-15 12:46:45 -0400

    Login with Google - Google side.

--- app/src/main/java/com/johnstrack/firebasesocial/MainActivity.kt
+++ app/src/main/java/com/johnstrack/firebasesocial/MainActivity.kt
@@ -1,12 +1,50 @@
-    package com.johnstrack.firebasesocial
+package com.johnstrack.firebasesocial
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val GOOGLE_SIGNIN_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        googleSigninBtn.setOnClickListener {
+            val signinIntent = googleSignInClient.signInIntent
+            startActivityForResult(signinIntent, GOOGLE_SIGNIN_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode === GOOGLE_SIGNIN_CODE) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+            } catch (e: ApiException) {
+                Log.e("Error", "Could not sign in Google: ${e.statusCode}" )
+            }
+        }
     }
 }
